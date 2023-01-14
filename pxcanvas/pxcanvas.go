7e0eb099a1655e71c9cd595ef310e9d01a1b11b2
unknown <ardhisparahitaa@gmail.com>
2023-01-14 20:43:38 +0700

    feat: add pxcanvas.go

---
+++ pxcanvas/pxcanvas.go
@@ -0,0 +1,64 @@
+package pxcanvas
+
+import (
+	"image"
+	"image/color"
+
+	"ardhisparahita.io/pixl/apptype"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/internal/widget"
+	"fyne.io/fyne/v2/widget"
+)
+
+type PxCanvasMouseState struct {
+	previousCoord *fyne.PointEvent
+}
+
+type PxCanvas struct {
+	widget.BaseWidget
+	apptype.PxCanvasConfig
+	renderer    *PxCanvasRenderer
+	pixelData   image.Image
+	mouseState  PxCanvasMouseState
+	appState    *apptype.State
+	reloadImage bool
+}
+
+func (PxCanvas *PxCanvas) Bounds() image.Rectangle {
+	x0 := int(PxCanvas.CanvasOffset.X)
+	y0 := int(PxCanvas.CanvasOffset.Y)
+	x1 := int(PxCanvas.PxCols*PxCanvas.PxSize + int(PxCanvas.CanvasOffset.X))
+	y1 := int(PxCanvas.PxRows*PxCanvas.PxSize + int(PxCanvas.CanvasOffset.Y))
+
+	return image.Rect(x0, y0, x1, y1)
+}
+
+func InBounds(pos *fyne.Position, bounds image.Rectangle) bool {
+	if pos.X >= float32(bounds.Min.X) &&
+		pos.X < float32(bounds.Max.X) &&
+		pos.Y >= float32(bounds.Min.Y) &&
+		pos.Y < float32(bounds.Max.Y) {
+		return true
+	}
+	return false
+}
+
+func NewBlankImage(cols, rows int, c color.Color) image.Image {
+	img := image.NewNRGBA(image.Rect(0, 0, cols, rows))
+	for y := 0; y < rows; y++ {
+		for x := 0; x < cols; x++ {
+			img.Set(x, y, c)
+		}
+	}
+	return img
+}
+
+func NewPxCanvas(state *apptype.State, config apptype.PxCanvasConfig) *PxCanvas {
+	PxCanvas := &PxCanvas{
+		PxCanvasConfig: config,
+		appState:       state,
+	}
+	PxCanvas.pixelData = NewBlankImage(config.PxCols, config.PxRows, color.NRGBA{128, 128, 128, 255})
+	PxCanvas.ExtendbaseWidget(PxCanvas)
+	return PxCanvas
+}
