4e7224e9d278c730cddd5d449ee3afe37cb60cd6
unknown <ardhisparahitaa@gmail.com>
2023-01-18 08:29:31 +0700

    feat: add mouse.go

---
+++ pxcanvas/mouse.go
@@ -0,0 +1,25 @@
+package pxcanvas
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/driver/desktop"
+)
+
+func (PxCanvas *PxCanvas) Scrolled(ev *fyne.ScrollEvent) {
+	PxCanvas.Scale(int(ev.Scrolled.DY))
+	PxCanvas.Refresh()
+}
+
+func (PxCanvas *PxCanvas) MouseMoved(ev *desktop.MouseEvent) {
+	PxCanvas.TryPan(PxCanvas.mouseState.previousCoord, ev)
+	PxCanvas.Refresh()
+	PxCanvas.mouseState.previousCoord = &ev.PointEvent
+}
+
+func (PxCanvas *PxCanvas) MouseIn(ev *desktop.MouseEvent) {
+
+}
+
+func (PxCanvas *PxCanvas) MouseOut() {
+
+}
