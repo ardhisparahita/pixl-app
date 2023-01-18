ce7347f5729f6e330c6ef0556c9304dc68c971fd
unknown <ardhisparahitaa@gmail.com>
2023-01-18 08:15:08 +0700

    add ops.go

---
+++ pxcanvas/ops.go
@@ -0,0 +1,25 @@
+package pxcanvas
+
+import "fyne.io/fyne/v2"
+
+func (PxCanvas *PxCanvas) Pan(previousCoord, currentCoord fyne.PointEvent) {
+	xDiff := currentCoord.Position.X - previousCoord.Position.X
+	yDiff := currentCoord.Position.Y - previousCoord.Position.Y
+
+	PxCanvas.CanvasOffset.X += xDiff
+	PxCanvas.CanvasOffset.Y += yDiff
+	PxCanvas.Refresh()
+}
+
+func (pxCanvas *PxCanvas) Scale(direction int) {
+	switch {
+	case direction > 0:
+		pxCanvas.PxSize += 1
+	case direction < 0:
+		if pxCanvas.PxSize > 2 {
+			pxCanvas.PxSize -= 1
+		}
+	default:
+		pxCanvas.PxSize = 10
+	}
+}
